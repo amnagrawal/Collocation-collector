a2ecd3ca09ec18be534169ae2c8c62ce4fa64fa5
amnagrawal <amnagrawal1994@gmail.com>
2020-08-05 02:35:42 -0500

    Calling API asynchronously

--- ngram_api.py
+++ ngram_api.py
@@ -1,46 +1,72 @@
 import pandas as pd
+import os, sys
 
-from utils import build_query, get_response, nounNounFinder
-from nltk import word_tokenize, pos_tag
+import aiohttp
+import asyncio
+from aiohttp import ClientSession
 
-# print(response.text)
+
+from utils import build_query, get_response, nounNounFinder, run_query
+
+read_dir = ["./adjNoun", "./verbNoun"]
+
+
+def getfiles(dir):
+    files = []
+    for (dirpath, _, filenames) in os.walk(dir):
+        for filename in filenames:
+            files.append(os.path.join(dirpath, filename))
+
+    sorted(files)
+    return files
+
+
+adj_noun_files = getfiles(read_dir[0])
+verb_noun_files = getfiles(read_dir[1])
+save_dir = "./nounNoun"
 filename = "./kmeans_nounNoun_essays_metaphors.csv"
 response_file = "response.txt"
-# tokens = ['flow', 'tax']	#dummy tokens
-# TODO: uncomment this when done with parsing the response
 data = pd.read_csv(filename, delimiter=',')
 sources = data.identified_sources
+
 tokens = set()
+# for file in adj_noun_files:
+#     nouns = list(pd.read_csv(file, delimiter='\t').iloc[:, 2])
+#     for noun in nouns:
+#         noun = str(noun).lower()
+#         tokens.add(noun)
+#
+# for file in verb_noun_files:
+#     nouns = list(pd.read_csv(file, delimiter='\t').iloc[:, 2])
+#     for noun in nouns:
+#         noun = str(noun).lower()
+#         tokens.add(noun)
+
 for i, row in enumerate(sources):
     if not pd.isna(row):
         all_sources = row.strip().split(';')
-        tokens.update(all_sources)
+        for source in all_sources:
+            tokens.add(str(source).lower())
 
-with open(response_file, 'w') as f:
-    print(f'Total: {len(tokens)}')
-    for i, token in enumerate(tokens):
-        query = build_query(token)
-        response = get_response(query)
-        print(i, end=" ")
-        if not i % 50:
-            print()
-        f.write(response.text)
 
-print("Response file written")
+print(f'No of Tokens: {len(tokens)}')
+tokens = sorted(tokens)
 
-response_df = pd.read_csv(response_file, header=None, delimiter='\t')
+
+async def print_responses():
+    async with ClientSession() as session:
+        result = await asyncio.gather(*[run_query(token, session) for token in tokens])
+        return result
+
+
+loop = asyncio.get_event_loop()
+bigrams = loop.run_until_complete(print_responses())
+loop.close()
+
 nounNounList = []
-for row in response_df.iterrows():
-    text = row[1][0]
-    tokenized_text = [word[:-2] for word in text.split()]
-    tagged = pos_tag(tokenized_text)
-    frequency = row[1][1]
-    sourceNoun, targetNoun = nounNounFinder(tagged)
-    if sourceNoun:
-        nounNounList.append((frequency, sourceNoun, targetNoun))
 
 print(len(nounNounList))
-with open('nounNoun1.txt', 'w') as f:
+with open('nounNoun.txt', 'w') as f:
     for row in nounNounList:
         f.write('\t'.join([str(i) for i in row]))
         f.write('\n')
--- utils.py
+++ utils.py
@@ -1,62 +1,100 @@
 import urllib
-
+from nltk import word_tokenize, pos_tag
 import requests
 
 
 def build_query(token):
-	encoded_query = urllib.parse.quote(token + ' *')
-	params = {'corpus': 'eng-us', 'query': encoded_query, 'topk': 100, 'format': 'tsv'}
-	params = '&'.join('{}={}'.format(name, value) for name, value in params.items())
-	query = 'https://api.phrasefinder.io/search?' + params
-	return query
+    encoded_query = urllib.parse.quote(token + ' *')
+    params = {'corpus': 'eng-us', 'query': encoded_query, 'topk': 100, 'format': 'tsv'}
+    params = '&'.join('{}={}'.format(name, value) for name, value in params.items())
+    query = 'https://api.phrasefinder.io/search?' + params
+    return query
 
 
-def get_response(query):
-	response = requests.get(query)
-	return response
+async def run_query(token, session):
+    try:
+        response = await get_response(token, session)
+        rows = response.split('\n')
+        result = []
+
+        for row in rows:
+            if len(row):
+                frequency, sourceNoun, targetNoun, text = parse_response(row)
+                if sourceNoun:
+                    result.append((frequency, sourceNoun, targetNoun, text))
+        return result
+
+    except Exception as err:
+        print(f'Exception occured in run_query: {err}')
+        pass
+
+
+async def get_response(token, session):
+    url = build_query(token)
+    async with session.get(url) as response:
+        assert response.status == 200
+        return await response.text()
+
+
+def parse_response(response_text):
+    response_text = response_text.split('\t')
+    text = response_text[0]
+    tokenized_text = [word[:-2] for word in text.split()]
+    try:
+        tagged = pos_tag(tokenized_text)
+        frequency = response_text[1]
+        sourceNoun, targetNoun = nounNounFinder(tagged)
+        if sourceNoun and len(sourceNoun) > 1 and len(targetNoun) > 1:
+            return frequency, sourceNoun, targetNoun, ' '.join(tokenized_text)
+    except Exception as err:
+        print(f'Exception occured in parse response: {err}')
+        print(response_text)
+        pass
+
+    return None, None, None, None
 
 
 def nounNounFinder(taggedText):
-	POScolumn = [item[1] for item in taggedText]
-	wordColumn = [item[0] for item in taggedText]
-	pattern = ['of', 'is', 'was', 'were', 'am', 'had', 'will', 'are', 'have']
+    POScolumn = [item[1] for item in taggedText]
+    wordColumn = [item[0] for item in taggedText]
+    pattern = ['of', 'is', 'was', 'were', 'am', 'had', 'will', 'are', 'have']
 
-	for i in range(len(POScolumn) - 1):
-		firstNounIndex = i
-		secondNounIndex = i + 1
+    for i in range(len(POScolumn) - 1):
+        firstNounIndex = i
+        secondNounIndex = i + 1
 
-		indexCheck = lambda x: (x < len(POScolumn))
-		isNoun = lambda x: indexCheck(x) and POScolumn[x].startswith('NN')
-		isDeterminant = lambda x: indexCheck(x) and POScolumn[x] == 'DT'
-		isPartOfPattern = lambda x: indexCheck(x) and wordColumn[x] in pattern
-		isAdjective = lambda x: indexCheck(x) and POScolumn[x] == 'JJ'
-		patternMatches = lambda x: isNoun(x) or isPartOfPattern(x) or isDeterminant(x) or isAdjective(x)
+        indexCheck = lambda x: (x < len(POScolumn))
+        isNoun = lambda x: indexCheck(x) and POScolumn[x].startswith('NN')
+        isDeterminant = lambda x: indexCheck(x) and POScolumn[x] == 'DT'
+        isPartOfPattern = lambda x: indexCheck(x) and wordColumn[x] in pattern
+        isAdjective = lambda x: indexCheck(x) and POScolumn[x] == 'JJ'
+        patternMatches = lambda x: isNoun(x) or isPartOfPattern(x) or isDeterminant(x) or isAdjective(x)
 
-		# Check for metaphor patterns consisting of only two consecutive nouns
-		if isNoun(firstNounIndex) and isNoun(secondNounIndex) and not isNoun(secondNounIndex + 1):
-			sourceNounIndex = secondNounIndex
-			targetNounIndex = firstNounIndex
-			return wordColumn[sourceNounIndex], wordColumn[targetNounIndex]
+        # Check for metaphor patterns consisting of only two consecutive nouns
+        if isNoun(firstNounIndex) and isNoun(secondNounIndex) and not isNoun(secondNounIndex + 1):
+            sourceNounIndex = secondNounIndex
+            targetNounIndex = firstNounIndex
+            return wordColumn[sourceNounIndex], wordColumn[targetNounIndex]
 
-		# Check for metaphor patterns of type 'noun-is-noun' and 'noun-of-noun'
-		if isNoun(firstNounIndex) and isPartOfPattern(secondNounIndex):
-			candidateFound = False
-			while patternMatches(secondNounIndex):
-				if not isNoun(secondNounIndex + 1):
-					if isNoun(secondNounIndex):
-						candidateFound = True
-						break
-				secondNounIndex += 1
+        # Check for metaphor patterns of type 'noun-is-noun' and 'noun-of-noun'
+        if isNoun(firstNounIndex) and isPartOfPattern(secondNounIndex):
+            candidateFound = False
+            while patternMatches(secondNounIndex):
+                if not isNoun(secondNounIndex + 1):
+                    if isNoun(secondNounIndex):
+                        candidateFound = True
+                        break
+                secondNounIndex += 1
 
-			if candidateFound:
-				# Only in case of 'noun-of-noun' pattern the first noun is the source
-				if 'of' in wordColumn[firstNounIndex:secondNounIndex + 1]:
-					sourceNounIndex = firstNounIndex
-					targetNounIndex = secondNounIndex
-				else:
-					sourceNounIndex = secondNounIndex
-					targetNounIndex = firstNounIndex
+            if candidateFound:
+                # Only in case of 'noun-of-noun' pattern the first noun is the source
+                if 'of' in wordColumn[firstNounIndex:secondNounIndex + 1]:
+                    sourceNounIndex = firstNounIndex
+                    targetNounIndex = secondNounIndex
+                else:
+                    sourceNounIndex = secondNounIndex
+                    targetNounIndex = firstNounIndex
 
-				return wordColumn[sourceNounIndex], wordColumn[targetNounIndex]
+                return wordColumn[sourceNounIndex], wordColumn[targetNounIndex]
 
-	return None, None
+    return None, None
